2b3d101e1a4a61e2fab2b8de60d7cfedca7548a9
pedrobarretto <pedro@barretto.com.br>
2023-07-18 11:54:50 -0300

    chore: update .env.example

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID,
-  databaseURL: process.env.NEXT_DATABASE_URL,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
 };
 
 // Initialize Firebase
